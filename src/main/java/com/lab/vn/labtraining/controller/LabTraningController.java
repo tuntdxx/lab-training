779dcf672b3498900175b6f02d0c9edf6f1ec02e
lost7 <nguyenhiep96vn@gmail.com>
2020-02-18 23:41:34 +0700

    demo controller

--- src/main/java/com/lab/vn/labtraining/controller/LabTraningController.java
+++ src/main/java/com/lab/vn/labtraining/controller/LabTraningController.java
@@ -1,10 +1,21 @@
 package com.lab.vn.labtraining.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @author <a href="mailto:nguyenhiep96vn@gmail.com">Hiep Nguyen Van</a>
  * @version 1.0.0
  * @date Feb 18, 2020
  */
+@Controller
+@RequestMapping("/api")
 public class LabTraningController {
-
+	
+	@GetMapping("/test")
+	public @ResponseBody String demo() {
+		return "Mai xoa di";
+	}
 }
