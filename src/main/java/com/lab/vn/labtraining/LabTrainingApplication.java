42a592d310cb454971739a3263ab3c9c581f6f86
lost7 <nguyenhiep96vn@gmail.com>
2020-02-18 22:40:11 +0700

    first commit

---
+++ src/main/java/com/lab/vn/labtraining/LabTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.lab.vn.labtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/vn/labtraining/LabTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.vn.labtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
